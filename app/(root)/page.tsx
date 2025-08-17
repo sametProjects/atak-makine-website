09683948b02132aa74baeeaec81c92381429f327
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-08-17 20:35:40 +0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default Homepage;
