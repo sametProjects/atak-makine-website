9bb8d5b42ebe4def3f5bd9d023d6030c31596485
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-08-18 22:18:35 +0300

    15-sample products & product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,16 @@
-import { Button } from "@/components/ui/button";
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
-const Homepage = () => {
+const HomePage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
-      <Button>Button</Button>
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
     </div>
   );
 };
-
-export default Homepage;
+export default HomePage;
