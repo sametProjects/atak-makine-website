02b9a3d19fa4d6659341c280b32416220ae503ac
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-08-21 20:29:25 +0300

    25-product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,16 +1,21 @@
 "use server";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // Get the latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
   return convertToPlainObject(data);
+}
+
+// Get single product by slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
