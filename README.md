23291ec8c38fdd45e55efa36769a36635e71a4b7
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-08-22 13:20:22 +0300

    30-prisma user-related models

--- README.md
+++ README.md
@@ -12,6 +12,18 @@ docker-compose down
 
 ## Prisma Komutları
 
+**npx prisma generate**
+
+Ne zaman çalıştırılır: schema.prisma dosyasında herhangi bir değişiklik yaptığında
+Ne yapar: TypeScript client kodunu yeniden oluşturur
+Neden gerekli: TypeScript'te yeni modellerin tiplerini kullanabilmek için
+
+**npx prisma migrate dev --name mesaj_buraya_yaz**
+
+Ne yapar: Hem migration dosyası oluşturur hem de veritabanına uygular.
+NOT: 'npx prisma db push' un alternatifidir. Bunu kullanıyorsan diğerini kullanmana gerek yok.
+NOT: Tek farkı migration dosyası oluşturur ve geçmişte yaptığın değişiklikleri görebilirsin.
+
 **npx prisma db push**
 
 Ne zaman çalıştırılır: Sadece schema.prisma dosyasında model değişikliği yaptığında
@@ -23,8 +35,4 @@ Ne yapar: Schema değişikliklerini database'e uygular (tablolar oluşturur/gün
 - Field tipini değiştirdiğinde
 - İlişki eklediğinde/değiştirdiğinde
 
-**npx prisma generate**
-
-Ne zaman çalıştırılır: schema.prisma dosyasında herhangi bir değişiklik yaptığında
-Ne yapar: TypeScript client kodunu yeniden oluşturur
-Neden gerekli: TypeScript'te yeni modellerin tiplerini kullanabilmek için
+npx prisma migrate dev --name add_user_based_tables
---
+++ prisma/migrations/20250822101533_add_user_based_tables/migration.sql
@@ -0,0 +1,62 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL DEFAULT 'NO_NAME',
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'user',
+    "emailVerified" TIMESTAMP(6),
+    "image" TEXT,
+    "address" JSON,
+    "paymentMethod" TEXT,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Account" (
+    "userId" UUID NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerAccountId" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "account_provider_providerAccountId_pk" PRIMARY KEY ("provider","providerAccountId")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Session" (
+    "sessionToken" TEXT NOT NULL,
+    "userId" UUID NOT NULL,
+    "expires" TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("sessionToken")
+);
+
+-- CreateTable
+CREATE TABLE "public"."VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("identifier","token")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_idx" ON "public"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."Account" ADD CONSTRAINT "account_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "public"."Session" ADD CONSTRAINT "session_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
