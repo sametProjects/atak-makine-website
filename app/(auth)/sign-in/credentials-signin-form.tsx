ca5caf047d8588c5c2b05d72069cad93e5d76437
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-08-22 19:47:50 +0300

    37-hook up sign in form

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -4,10 +4,26 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
+import { useFormState, useFormStatus } from "react-dom";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useFormState(signInWithCredentials, {
+    message: "",
+    success: false,
+  });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button disabled={pending} className="w-full" variant="default">
+        {pending ? "Signing In..." : "Sign In with credentials"}
+      </Button>
+    );
+  };
+
   return (
-    <form>
+    <form action={action}>
       <div className="space-y-6">
         <div>
           <Label htmlFor="email">Email</Label>
@@ -32,10 +48,12 @@ const CredentialsSignInForm = () => {
           />
         </div>
         <div>
-          <Button className="w-full" variant="default">
-            Sign In with credentials
-          </Button>
+          <SignInButton />
         </div>
+
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
 
         <div className="text-sm text-center text-muted-foreground">
           Don&apos;t have an account?{" "}
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -10,12 +10,19 @@ import {
 } from "@/components/ui/card";
 import { APP_NAME } from "@/lib/constants";
 import CredentialsSignInForm from "./credentials-signin-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-const SignInPage = () => {
+const SignInPage = async () => {
+  const session = await auth();
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <Card>
