8e42fe70bbf9156b9d4e3826308528229954d06a
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-08-27 08:20:21 +0300

    55-cart page

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import { Cart } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+
+const CartTable = ({ cart }: { cart?: Cart }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">Table</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CartTable;
