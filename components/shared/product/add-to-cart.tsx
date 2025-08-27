72e0a3193b1dfef523f6b8b226f56f08aba2762b
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-08-27 07:39:18 +0300

    52-dynamic cart button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
+import { Plus, Minus } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
-import { addItemToCart } from "@/lib/actions/cart.actions";
-import { CartItem } from "@/types";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { Cart, CartItem } from "@/types";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
   const { toast } = useToast();
 
@@ -34,9 +34,35 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     });
   };
 
-  return (
+  // Remove item from cart
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+
+    toast({
+      variant: res.success ? "default" : "destructive",
+      description: res.message,
+    });
+
+    return;
+  };
+
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="w-4 h-4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="w-4 h-4" />
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus /> Add To Cart
+      <Plus className="w-4 h-4" />
+      Add to cart
     </Button>
   );
 };
