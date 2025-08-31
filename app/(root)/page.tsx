3aef2f8541ea5c07dacfa3fec12fdc0040e546ea
Ahmet Samet Demiralay <ahmetsametdemiralay@samco.local>
2025-08-31 16:47:10 +0300

    vps server and docker setup v3

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,14 @@ import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
 
 const HomePage = async () => {
-  const latestProducts = await getLatestProducts();
+  // const latestProducts = await getLatestProducts();
+  //*** build de hata verdigi icin usttekini yoruma alip burayi ekledim ***
+  const latestProducts = (await getLatestProducts()).map((p) => ({
+    ...p,
+    price: p.price.toString(),
+    rating: p.rating.toString(),
+  }));
+  //*** build de hata verdigi icin usttekini yoruma alip burayi ekledim ***
 
   return (
     <div className="space-y-8">
